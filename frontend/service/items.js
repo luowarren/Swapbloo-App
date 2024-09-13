d4595d2f8e039ac1d7894db24efc3471caa8c4ec
Adeline Chen <adelinechen13@gmail.com>
2024-09-13 10:16:51 +1000

    uncomment files

--- frontend/service/items.js
+++ frontend/service/items.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { createClient } from '@supabase/supabase-js'; // Correct named import
 
 // Load environment variables from .env file
-// dotenv.config({ path: ".env" }); // Optional: specify the path to .env
+dotenv.config({ path: ".env" }); // Optional: specify the path to .env
 
 const SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51eW5pdmJwbnVsem5qY210dnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNTk5MTEsImV4cCI6MjAzODczNTkxMX0.H-2tACfryiR97R5kQjas7RUaTBf2RpdnDgq-OGmfZzU'
 const SUPABASE_URL='https://nuynivbpnulznjcmtvpq.supabase.co'
