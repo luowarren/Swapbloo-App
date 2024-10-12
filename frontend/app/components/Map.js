0c581c17e102e66f96ca027bde30fb85f5fcf9ed
Adeline Chen <adelinechen13@gmail.com>
2024-10-12 21:22:37 +1000

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit 349074d0547858eba19cd75d39683c1d2b860581, reversing
    changes made to 2030dd284d27173a580da54db12ccd73d37c0739.

--- frontend/app/components/Map.js
+++ frontend/app/components/Map.js
@@ -58,37 +58,6 @@ export default function ShowMap({
     );
   }, []);
 
-  useEffect(() => {
-    setSelected(selectedLocation)
-  },[selectedLocation])
-
-  function sameLoc(loc) {
-    return areObjectsEqual(loc, selected);
-  }
-  function areObjectsEqual(obj1, obj2) {
-    if (
-      typeof obj1 !== "object" ||
-      obj1 === null ||
-      typeof obj2 !== "object" ||
-      obj2 === null
-    ) {
-      return false;
-    }
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-
-    if (keys1.length !== keys2.length) {
-      return false;
-    }
-
-    for (const key of keys1) {
-      if (obj1[key] !== obj2[key]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   // Search functionality
   useEffect(() => {
     if (searchTerm) {
